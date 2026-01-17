50ad93b667c6fdd9c0028c5e959744a490283a7f
Yannsean22 <69013397+Yannsean22@users.noreply.github.com>
2026-01-16 19:30:27 -0500

    Delete Desktop directory

--- Desktop/preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("electron", {
-    minimizeWindow: () => ipcRenderer.send("window-minimize"),
-    maximizeWindow: () => ipcRenderer.send("window-maximize"),
-    closeWindow: () => ipcRenderer.send("window-close"),
-    restartApp: () => ipcRenderer.send("restart-app"), // Expose the restart function
-});
